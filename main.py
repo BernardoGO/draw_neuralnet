b77abc9dc9c4fd513094c8ed34adb390488ff788
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-07-16 16:20:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+import core.layer
