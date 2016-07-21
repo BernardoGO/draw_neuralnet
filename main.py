61182f5bb7928c5b2c93611287eb6c437497b6a2
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-07-21 16:26:07 -0300

    save figure + collection

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 import core.layer
 
-def new():
-    patches = []
-    colors = []
+def generate():
+    colors += [0, 1]
+    print(["P", len(patches), len(colors)])
+    #print(patches)
+    collection = PatchCollection(patches, cmap=plt.cm.gray)
+    collection.set_array(np.array(colors))
+    ax.add_collection(collection)
+    #ax.add_line(Line2D([0, 50],
+    #                      [0, -16]))
     plt.tight_layout()
-    plt.axis('equal')
     #plt.ylim([0,80])
-
+    plt.xlim([0,model.getCount() * core.layer.get_effective_area()])
     plt.axis('off')
     plt.show()
+
+    fig_dir = './'
+    fig_ext = '.png'
+    fig.savefig(os.path.join(fig_dir, 'figure' + fig_ext),
+                bbox_inches='tight', pad_inches=0)
