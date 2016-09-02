f219f6735d2b40a2cb8d762b2d3f40a44e2523fd
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-09-02 16:41:09 -0300

    layer base script

---
+++ layer_base.py
@@ -0,0 +1,6 @@
+import numpy as np
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+import keras_layers
+import sys
