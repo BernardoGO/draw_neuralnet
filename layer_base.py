9c24fa308f4411acb8ff88e4af2f3f7546aef2ff
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-09-04 16:41:57 -0300

    layer class

--- layer_base.py
+++ layer_base.py
@@ -4,3 +4,13 @@ from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 import keras_layers
 import sys
+
+
+lass layer():
+    def __init__(self,visible_top, visible_bottom,visible_left,visible_right,size_x,size_y):
+        self.visible_top = visible_top
+        self.visible_bottom = visible_bottom
+        self.visible_left = visible_left
+        self.visible_right = visible_right
+        self.size_x = size_x
+        self.size_y = size_y
