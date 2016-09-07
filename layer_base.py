ac1720b3a4a811bd5e9fce3d9366204102082d0b
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-09-07 16:42:30 -0300

    adding positions to layer

--- layer_base.py
+++ layer_base.py
@@ -6,11 +6,14 @@ import keras_layers
 import sys
 
 
-lass layer():
+class layer():
     def __init__(self,visible_top, visible_bottom,visible_left,visible_right,size_x,size_y):
         self.visible_top = visible_top
         self.visible_bottom = visible_bottom
         self.visible_left = visible_left
         self.visible_right = visible_right
+        self.actual_left = float('inf')
+        self.actual_top = float('-inf')
+        self.actual_bottom = float('-inf')
         self.size_x = size_x
         self.size_y = size_y
