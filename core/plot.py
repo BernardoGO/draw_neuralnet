8853942d05a551f2d2f5d15919c1d03673c33e6c
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-04-21 18:25:12 -0300

    updating main to use core.plot

--- core/plot.py
+++ core/plot.py
@@ -3,15 +3,31 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
-
+import core.plot
+import numpy as np
 
 patches = []
 colors = []
 
 
 def initPlot():
-    plt.rcdefaults()
+    core.plot.plt.rcdefaults()
 
 
-    fig, ax = plt.subplots()
-    return [fig, ax, patches, colors]
+    core.plot.fig, core.plot.ax = core.plot.plt.subplots()
+
+def show(model):
+    core.plot.colors += [0, 1]
+    print(["P", len(patches), len(colors)])
+    #print(patches)
+    collection = PatchCollection(patches, cmap=core.plot.plt.cm.gray)
+    collection.set_array(np.array(colors))
+    core.plot.ax.add_collection(collection)
+    #ax.add_line(Line2D([0, 50],
+    #                      [0, -16]))
+    core.plot.plt.tight_layout()
+    core.plot.plt.axis('equal')
+    #plt.ylim([0,80])
+    core.plot.plt.xlim([0,model.getCount() * core.layer.get_effective_area()])
+    core.plot.plt.axis('off')
+    core.plot.plt.show()
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import numpy as np
 import core.layer
 import core.plot
 
-
+from frameworks.keras.layers import Conv2D
+from frameworks.keras.model import Sequencial
 
 def new():
 
 
-    from frameworks.keras.layers import Conv2D
-    from frameworks.keras.model import Sequencial
-    import core.layer
+    core.plot.initPlot()
+
     model = Sequencial()
     model.add(Conv2D(32, (1, 1), input_shape=(500,500,3), trainable=False))
     model.add(Conv2D(2, (3, 3), trainable=False))
@@ -20,7 +20,7 @@ def new():
 
     for ind in range(len(model.layers)):
 
-        core.layer.add_layer(patches, colors, size=model.layers[ind].getSize(),
+        core.layer.add_layer(core.plot.patches, core.plot.colors, size=model.layers[ind].getSize(),
                   num=model.layers[ind].getFilters(),
                   position = model.layers[ind].position,
                   titleText = model.layers[ind].titleText+ '\n{}@{}x{}'.format(
@@ -28,7 +28,7 @@ def new():
                   mappingText = model.layers[ind].mappingText + '\n{}x{} kernel'.format(
                           model.layers[ind].getSize(), model.layers[ind].getSize())
                   )
-        core.layer.label(ind, plt, top=True)
+        core.layer.label(ind, core.plot.plt, top=True)
 
 
 
@@ -38,27 +38,15 @@ def new():
 
     for ind in range(len(core.layer.layers)-1):
         #print([len(core.layer.positions), ind])
-        core.layer.add_mapping(patches, colors, start_ratio_list[ind],
-                    patch_size_list[ind], ind, ax
+        core.layer.add_mapping(core.plot.patches, core.plot.colors, start_ratio_list[ind],
+                    patch_size_list[ind], ind, core.plot.ax
                     )
-        core.layer.label(ind, plt, top=False)
+        core.layer.label(ind, core.plot.plt, top=False)
 
     ################
-    colors += [0, 1]
-    print(["P", len(patches), len(colors)])
-    #print(patches)
-    collection = PatchCollection(patches, cmap=plt.cm.gray)
-    collection.set_array(np.array(colors))
-    ax.add_collection(collection)
-    #ax.add_line(Line2D([0, 50],
-    #                      [0, -16]))
-    plt.tight_layout()
-    plt.axis('equal')
-    #plt.ylim([0,80])
-    plt.xlim([0,model.getCount() * core.layer.get_effective_area()])
-    plt.axis('off')
-    plt.show()
-    #fig.set_size_inches(8, 2.5)
+    core.plot.show(model)
+
+
 
 
 
