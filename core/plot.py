668e68944d0df8625aff7bba584a99e2279aff01
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-04-23 18:27:01 -0300

    generate model on core.plot

--- core/plot.py
+++ core/plot.py
@@ -31,3 +31,29 @@ def show(model):
     core.plot.plt.xlim([0,model.getCount() * core.layer.get_effective_area()])
     core.plot.plt.axis('off')
     core.plot.plt.show()
+
+def gen(model):
+    for ind in range(len(model.layers)):
+
+        core.layer.add_layer(core.plot.patches, core.plot.colors, size=model.layers[ind].getSize(),
+                  num=model.layers[ind].getFilters(),
+                  position = model.layers[ind].position,
+                  titleText = model.layers[ind].titleText+ '\n{}@{}x{}'.format(
+                      model.layers[ind].getFilters(),  model.layers[ind].input_shape[0], model.layers[ind].input_shape[1]),
+                  mappingText = model.layers[ind].mappingText + '\n{}x{} kernel'.format(
+                          model.layers[ind].getSize(), model.layers[ind].getSize())
+                  )
+        core.layer.label(ind, core.plot.plt, top=True)
+
+
+
+    start_ratio_list = [[0.2, 0.1], [0.2, 0.3], [0.4, 0.5], [0.4, 0.8]]
+    patch_size_list = [2, 3, 5, 2]
+    ind_bgn_list = range(len(patch_size_list))
+
+    for ind in range(len(core.layer.layers)-1):
+        #print([len(core.layer.positions), ind])
+        core.layer.add_mapping(core.plot.patches, core.plot.colors, start_ratio_list[ind],
+                    patch_size_list[ind], ind, core.plot.ax
+                    )
+        core.layer.label(ind, core.plot.plt, top=False)
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 import os
 import numpy as np
-
 import core.layer
 import core.plot
-
 from frameworks.keras.layers import Conv2D
 from frameworks.keras.model import Sequencial
 
@@ -18,30 +16,7 @@ def new():
     model.add(Conv2D(2, (3, 3), trainable=False))
     model.add(Conv2D(4, (3, 3), trainable=False))
 
-    for ind in range(len(model.layers)):
-
-        core.layer.add_layer(core.plot.patches, core.plot.colors, size=model.layers[ind].getSize(),
-                  num=model.layers[ind].getFilters(),
-                  position = model.layers[ind].position,
-                  titleText = model.layers[ind].titleText+ '\n{}@{}x{}'.format(
-                      model.layers[ind].getFilters(),  model.layers[ind].input_shape[0], model.layers[ind].input_shape[1]),
-                  mappingText = model.layers[ind].mappingText + '\n{}x{} kernel'.format(
-                          model.layers[ind].getSize(), model.layers[ind].getSize())
-                  )
-        core.layer.label(ind, core.plot.plt, top=True)
-
-
-
-    start_ratio_list = [[0.2, 0.1], [0.2, 0.3], [0.4, 0.5], [0.4, 0.8]]
-    patch_size_list = [2, 3, 5, 2]
-    ind_bgn_list = range(len(patch_size_list))
-
-    for ind in range(len(core.layer.layers)-1):
-        #print([len(core.layer.positions), ind])
-        core.layer.add_mapping(core.plot.patches, core.plot.colors, start_ratio_list[ind],
-                    patch_size_list[ind], ind, core.plot.ax
-                    )
-        core.layer.label(ind, core.plot.plt, top=False)
+    core.plot.gen(model)
 
     ################
     core.plot.show(model)
